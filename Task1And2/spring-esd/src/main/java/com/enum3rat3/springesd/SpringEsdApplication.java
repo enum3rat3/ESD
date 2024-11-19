54ca62c8b69bb6eb7c345bafda600550a65317e0
enum3rat3 <jaiminjadvani3728@gmail.com>
2024-11-19 17:22:45 +0530

    Repo For Task 1 and 2 created

---
+++ Task1And2/spring-esd/src/main/java/com/enum3rat3/springesd/SpringEsdApplication.java
@@ -0,0 +1,13 @@
+package com.enum3rat3.springesd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEsdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEsdApplication.class, args);
+    }
+
+}
---
+++ Task1And2/spring-esd/src/test/java/com/enum3rat3/springesd/SpringEsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enum3rat3.springesd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEsdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
